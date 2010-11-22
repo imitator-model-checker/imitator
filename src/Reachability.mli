f71268b3d77598ca815695034b3ddc3c332fe7a9
Romain Soulat <romain.soulat@thalesgroup.com>
2010-11-22 09:30:14 +0000

    Added the dynamic code to perform on-the-fly intersection
    git-svn-id: https://svn.lsv.ens-cachan.fr/kuehne/projects/IMITATOR2/branches/pta@176 6f1578d2-a1e8-4f7d-a79a-10a3a1991f00

--- src/Reachability.mli
+++ src/Reachability.mli
@@ -3,12 +3,13 @@
  *                     IMITATOR II
  * 
  * Laboratoire Specification et Verification (ENS Cachan & CNRS, France)
- * Author:        Etienne Andre, Ulrich Kuehne
+ * Author:        Etienne Andre, Ulrich Kuehne, Romain Soulat
  * Created:       2010/07/22
- * Last modified: 2010/07/22
+ * Last modified: 2010/11/04
  *
  **************************************************)
 
+open Global
 open AbstractImitatorFile
 open Options
 open Graph
@@ -16,9 +17,25 @@ open LinearConstraint
 
 val create_initial_state : abstract_program -> state
 
-val post : abstract_program -> pi0 -> reachability_graph -> state_index -> state_index list
+val post : abstract_program -> imitator_options -> pi0 -> reachability_graph -> state_index -> state_index list
 
-val post_star : abstract_program -> imitator_options -> pi0 -> state -> 
-					 		 reachability_graph * int * float
-	
+(*val post_star : abstract_program -> imitator_options -> pi0 -> state -> 
+					 		 -> reachability_graph * int * float*)				 		 
+(*         val post_star :
+           AbstractImitatorFile.abstract_program ->
+           < dynamic : bool; post_limit : int option;
+             time_limit : int option; .. > ->
+           (LinearConstraint.variable -> LinearConstraint.coef) ->
+           Automaton.location * LinearConstraint.linear_constraint ->
+           LinearConstraint.linear_constraint * Graph.reachability_graph *
+           int * float
+*)
+         val post_star :
+           abstract_program -> imitator_options -> pi0 -> state ->
+           (* K0 * reachability_graph * nb_iterations * counter_value *)
+           returned_constraint * Graph.reachability_graph * int * float
+
+
+
+
 val print_stats: unit -> unit
