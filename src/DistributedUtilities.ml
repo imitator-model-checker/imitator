575b195fc70226ec99206ce6382763cfa266efef
Etienne Andre <Etienne.Andre@univ-paris13.fr>
2014-04-24 12:29:53 +0000

    added a new print to solve the PaTATOR bug (Camille c a toi)
    git-svn-id: https://svn.lsv.ens-cachan.fr/kuehne/projects/IMITATOR2/trunk@842 6f1578d2-a1e8-4f7d-a79a-10a3a1991f00

--- src/DistributedUtilities.ml
+++ src/DistributedUtilities.ml
@@ -390,7 +390,8 @@ let receive_pull_request () =
 		print_message Debug_medium ("[Master] Buffer created with length " ^ (string_of_int len));
 		let res = Mpi.receive source_rank (int_of_slave_tag Slave_result_tag) Mpi.comm_world in
 		print_message Debug_medium ("[Master] Reception done");
-(* 		print_string res.(0); *)
+		print_char res.[0];
+		print_message Debug_medium ("[Master] Coucou j'ai ecrit le premier caractere !");
 
 		(* Print some information *)
 		if debug_mode_greater Debug_medium then
