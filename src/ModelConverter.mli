8a5fcbb1eef6d5ec5e3a4c4c0323f9247246b7cf
Étienne André <Etienne.Andre@univ-paris13.fr>
2014-10-02 18:39:50 +0200

    add a warning to improve the interface of ModelConverter

--- src/ModelConverter.mli
+++ src/ModelConverter.mli
@@ -7,7 +7,7 @@
  * Laboratoire Specification et Verification (ENS Cachan & CNRS, France)
  * Author:        Etienne Andre
  * Created:       2009/09/09
- * Last modified: 2012/06/15
+ * Last modified: 2014/10/02
  *
  ****************************************************************)
 
@@ -33,4 +33,7 @@ exception InvalidPi0
 (** Functions *)
 (****************************************************************)
 (* Convert the parsing structure into an abstract model *)
+
+(*** WARNING: should better return a structure PI0 OR v0 ***)
+
 val abstract_model_of_parsing_structure : ParsingStructure.parsing_structure ->  ParsingStructure.pi0 -> ParsingStructure.v0 ->  imitator_options -> (abstract_model * pi0 * v0)
