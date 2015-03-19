fd30113488cfa0e89f3e4a9ba7b62ccaa8f3aed5
Étienne André <Etienne.Andre@univ-paris13.fr>
2015-03-19 16:27:19 +0100

    Now IMITATOR 2.6.3 (due to change in syntax)

--- src/Constants.ml
+++ src/Constants.ml
@@ -7,13 +7,13 @@
  * Author:        Etienne Andre
  * 
  * Created:       2014/10/24
- * Last modified: 2014/10/24
+ * Last modified: 2015/03/19
  *
  ****************************************************************)
  
 
 let program_name = "IMITATOR"
-let version_string = "2.6.2"
+let version_string = "2.6.3"
 
 (* Extension for input model files *)
 let model_extension = ".imi"
