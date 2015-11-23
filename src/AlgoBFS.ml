b5c2badc7aaaea448dce4da3207004cff64dcbbd
Étienne André <Etienne.Andre@univ-paris13.fr>
2015-11-23 18:15:32 +0100

    New module AlgoBFS and minor modifs

---
+++ src/AlgoBFS.ml
@@ -0,0 +1,31 @@
+(************************************************************
+ *
+ *                       IMITATOR
+ * 
+ * Laboratoire Spécification et Vérification (ENS Cachan & CNRS, France)
+ * LIPN, Université Paris 13, Sorbonne Paris Cité (France)
+ * 
+ * Module description: main class to explore the state space in breadth-first search manner
+ * 
+ * File contributors : Étienne André
+ * Created           : 2015/11/23
+ * Last modified     : 2015/11/23
+ *
+ ************************************************************)
+
+
+(**************************************************************)
+(* Modules *)
+(**************************************************************)
+
+
+(**************************************************************)
+(* Class definition *)
+(**************************************************************)
+class algoBFS =
+	object (self)
+
+	method run () =
+		(*** HACK: just a test ***)
+		Result.Noresultbecausethatsatest
+end;;
---
+++ src/AlgoBFS.mli
@@ -0,0 +1,27 @@
+(************************************************************
+ *
+ *                       IMITATOR
+ * 
+ * Laboratoire Spécification et Vérification (ENS Cachan & CNRS, France)
+ * LIPN, Université Paris 13, Sorbonne Paris Cité (France)
+ * 
+ * Module description: main class to explore the state space in breadth-first search manner
+ * 
+ * File contributors : Étienne André
+ * Created           : 2015/11/23
+ * Last modified     : 2015/11/23
+ *
+ ************************************************************)
+
+
+(**************************************************************)
+(* Modules *)
+(**************************************************************)
+
+(**************************************************************)
+(* Class definition *)
+(**************************************************************)
+class algoBFS :
+	object
+		method run : unit -> Result.imitator_result
+end
