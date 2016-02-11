e659193dd3e7f88d05734b80c7cb76d617af8ca1
Étienne André <Etienne.Andre@univ-paris13.fr>
2016-02-11 10:29:11 +0100

    Fix a bug in the constraint inclusion

--- src/LinearConstraint.ml
+++ src/LinearConstraint.ml
@@ -2608,7 +2608,8 @@ let p_nnconvex_constraint_is_pi0_compatible pval p_nnconvex_constraint =
 (** Check if a nnconvex_constraint is included in another one *)
 let p_nnconvex_constraint_is_leq p_nnconvex_constraint p_nnconvex_constraint' =
 	(*** TODO: counter ***)
-	ppl_Pointset_Powerset_NNC_Polyhedron_contains_Pointset_Powerset_NNC_Polyhedron p_nnconvex_constraint p_nnconvex_constraint'
+	(*** NOTE: PPL works in the reverse order: the 2nd contains the 1st one ***)
+	ppl_Pointset_Powerset_NNC_Polyhedron_contains_Pointset_Powerset_NNC_Polyhedron p_nnconvex_constraint' p_nnconvex_constraint
 	(*** TODO: counter ***)
 
 
