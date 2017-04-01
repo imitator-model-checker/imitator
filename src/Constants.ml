5c529786ab05ebebb4ee11e29b60c181a14fb3b3
Étienne André <Etienne.Andre@univ-paris13.fr>
2017-04-01 07:43:53 +0200

    Add April 1st new feature: a -romeo mode; version now 2.9.1

--- src/Constants.ml
+++ src/Constants.ml
@@ -9,7 +9,7 @@
  * 
  * File contributors : Étienne André
  * Created           : 2014/10/24
- * Last modified     : 2017/03/24
+ * Last modified     : 2017/04/01
  *
  ************************************************************)
 
@@ -22,7 +22,7 @@
 
 let program_name = "IMITATOR"
 
-let version_string = "2.9.1-working"
+let version_string = "2.9.1"
 
 let version_name = "Butter Incaberry"
 
