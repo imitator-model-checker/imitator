11f7d0fac1696dc6e93006288726caca381e2cf6
Hoang Gia NGUYEN <hoangia90@gmail.com>
2017-10-12 01:32:24 +0200

    fixed few bugs and Tesed new functions

--- src/AlgoNZCUBdist.ml
+++ src/AlgoNZCUBdist.ml
@@ -48,248 +48,135 @@ let continuous_part_of_guard (*: LinearConstraint.pxd_linear_constraint*) = func
 	| Discrete_continuous_guard discrete_continuous_guard -> discrete_continuous_guard.continuous_guard
 
 
-let increase current_setup_index max_index = 
-
-	let current_setup_index = ref current_setup_index in
-
+let increase setup_index max_index = 
+	let current_setup_index = ref setup_index in
 	let current_dimension = ref 0 in
-
 	let not_is_max = ref true in
-
 		while !not_is_max do
-
 			(* Try to increment the local dimension *)
 			let current_dimension_incremented = !current_setup_index.(!current_dimension) + 1 in
-
 			if current_dimension_incremented <= max_index.(!current_dimension) then (
-
 				(* Increment this dimension *)
 				!current_setup_index.(!current_dimension) <- current_dimension_incremented;
-
 				not_is_max := false;
 			)
-
 			else ( 
 				(*reset*)
 				!current_setup_index.(!current_dimension) <- 0;
-
 				current_dimension := !current_dimension + 1;
-
 				if !current_dimension > (Array.length max_index -1) then(
-
 					not_is_max := false;
-
 				)
-
 			);
-
 		done;
-
 !current_setup_index
 
 
-
 let compare current_setup_index max_index = 
-
 	let b = ref true in
-
 	for i = 0 to (Array.length max_index -1) do
-
-  		if ( current_setup_index.(i) != max_index.(i) ) 
-
-  		then 
-
-  			b := false;
-
+  		if ( current_setup_index.(i) != max_index.(i) ) then b := false;
 	done;
-
 !b
 
 
+(* Testing - OK *)
 let decentralized_initial_loc model initial_global_location =
-
-	print_message Verbose_low ("decentralized_initial_loc function \n");
-
+	print_message Verbose_low ("Decentralizing initial global location \n");
 	(* let location_index = initial_location in *)
-
 	let init_constr_loc = ref [||] in
-	
-
-	List.iter (fun automaton_index -> print_message Verbose_low ("Automaton: " ^ (model.automata_names automaton_index) );
-
-				let temp = ref [||] in
-
-					let location_index = Location.get_location initial_global_location automaton_index in
-
-	        		print_message Verbose_low ("\n");
-
-	        		print_message Verbose_low (" Initial location name: " ^ (model.location_names automaton_index location_index) ) ;
-
-	        		let invariant1 = model.invariants automaton_index location_index in
-	        
-	                print_message Verbose_low ("   Invariant(Initial location): " ^ (LinearConstraint.string_of_pxd_linear_constraint model.variable_names invariant1 ) )  ; 
-							
-
-							print_message Verbose_low ("\n");
-
-	                	(*Checking bounded clocked in guards (Transition)*)
-	                	List.iter (fun action_index -> print_message Verbose_low (" Transition/Action: " ^ (model.action_names action_index) );
-	            
-	                    	List.iter (fun (guard, clock_updates, _, target_location_index) 
-	                    		-> 
-	                    		print_message Verbose_low ("   Guard: " ^ (LinearConstraint.string_of_pxd_linear_constraint model.variable_names (  continuous_part_of_guard guard ) ));	
-
-	                        	(** WORK HERE **)
-
-	                        	let invariant2 = model.invariants automaton_index target_location_index in
-
-	                        	
-								print_message Verbose_low ("\n");
-	                			print_message Verbose_low (" Location(Target location): " ^ (model.location_names automaton_index target_location_index) ) ;
-	                			print_message Verbose_low ("   Invariant(D): " ^ (LinearConstraint.string_of_pxd_linear_constraint model.variable_names invariant2 ) ) ;
-	                			print_message Verbose_low ("	  ----Map:(" 
-	                											^ (model.location_names automaton_index location_index) 
-	                											^ ")--" ^ (model.action_names action_index) ^ "-->(" 
-	                											^ (model.location_names automaton_index target_location_index) 
-	                											^ ") ----" );
-	                			print_message Verbose_low ("\n");
-	                			
-
-	                        	let clock_updates = match clock_updates with
-	                        						  No_update -> []
-													| Resets clock_update -> clock_update
-													| Updates clock_update_with_linear_expression -> raise (InternalError(" Clock_update are not supported currently! ")); in
-
-								(* init_constr_loc := ( Array.append !init_constr_loc [|(automaton_index, target_location_index, guard)|] ); *)
-								temp := ( Array.append !temp [|(automaton_index, target_location_index, guard)|] );
-								
-
-								()
-
-	                    	) (model.transitions automaton_index location_index action_index); 
-
-	                	) (model.actions_per_location automaton_index location_index); 
-
-	            		 print_message Verbose_low ("----------------End checking " ^ (model.location_names automaton_index location_index) ^ "---------------------");
-
-	            		 print_message Verbose_low ("\n");
-
-	        print_message Verbose_low ("\n");
-
+	List.iter (fun automaton_index -> 
+		print_message Verbose_low ("Automaton: " ^ (model.automata_names automaton_index) );
+		let temp = ref [||] in
+		let location_index = Location.get_location initial_global_location automaton_index in
+		print_message Verbose_low ("\n");
+		print_message Verbose_low (" Initial location name: " ^ (model.location_names automaton_index location_index) ) ;
+		let invariant1 = model.invariants automaton_index location_index in
+        print_message Verbose_low ("   Invariant (Initial location): " ^ (LinearConstraint.string_of_pxd_linear_constraint model.variable_names invariant1 ) )  ; 
+			print_message Verbose_low ("\n");
+        	(*Checking bounded clocked in guards (Transition)*)
+        	List.iter (fun action_index -> print_message Verbose_low (" Transition: " ^ (model.action_names action_index) );
+            	List.iter (fun (guard, clock_updates, _, target_location_index) -> 
+            		print_message Verbose_low ("   Guard: " ^ (LinearConstraint.string_of_pxd_linear_constraint model.variable_names (  continuous_part_of_guard guard ) ));	
+                	let invariant2 = model.invariants automaton_index target_location_index in
+					print_message Verbose_low ("\n");
+        			print_message Verbose_low (" Location(Target location): " ^ (model.location_names automaton_index target_location_index) ) ;
+        			print_message Verbose_low ("   Invariant(D): " ^ (LinearConstraint.string_of_pxd_linear_constraint model.variable_names invariant2 ) ) ;
+        			print_message Verbose_low ("	  ----Map:(" 
+        											^ (model.location_names automaton_index location_index) 
+        											^ ")--" ^ (model.action_names action_index) ^ "-->(" 
+        											^ (model.location_names automaton_index target_location_index) 
+        											^ ") ----" );
+        			print_message Verbose_low ("\n");
+                	let clock_updates = match clock_updates with
+                						  No_update -> []
+										| Resets clock_update -> clock_update
+										| Updates clock_update_with_linear_expression -> raise (InternalError(" Clock_update are not supported currently! ")); in
+					(* init_constr_loc := ( Array.append !init_constr_loc [|(automaton_index, target_location_index, guard)|] ); *)
+					temp := ( Array.append !temp [|(automaton_index, target_location_index, guard)|] );
+					()
+            	) (model.transitions automaton_index location_index action_index); 
+        	) (model.actions_per_location automaton_index location_index); 
+    		print_message Verbose_low ("----------------End checking " ^ (model.location_names automaton_index location_index) ^ "---------------------");
+    		print_message Verbose_low ("\n");
 	        init_constr_loc := ( Array.append !init_constr_loc [|!temp|] );
-
 	) model.automata;
-	print_message Verbose_low ("lenght!!!! " ^ string_of_int (Array.length !init_constr_loc) );
-
+	(* print_message Verbose_low ("lenght!!!! " ^ string_of_int (Array.length !init_constr_loc) ); *)
 !init_constr_loc
 
-
-
+(* Testing - OK *)
 
 let init_state_list model initial_loc_array = 
-
 	print_message Verbose_low (" init_state_list function ");
-
 	let max_index = ref [||] in
-
-	let number_of_setups = ref 0 in 
-
 	(* count from 0 *)
 	Array.iter ( fun sub_array -> 
-
 		let len = (Array.length sub_array) -1 in
-
-		print_message Verbose_low (" len sub array " ^ string_of_int (len) );
-
-		number_of_setups := !number_of_setups + (Array.length sub_array);
-
-		print_message Verbose_low (" Number of initial state setups " ^ string_of_int (!number_of_setups) );
-
+		(* print_message Verbose_low (" len sub array " ^ string_of_int (len) ); *)
 		max_index := ( Array.append !max_index [|len|] );
-
 	) initial_loc_array;
-
-	let current_setup_index = ref (Array.create (Array.length initial_loc_array) 0) in
-
-	
-
-	Array.iter ( fun element -> 
-
-		print_message Verbose_low (" element " ^ string_of_int (element) );
-
-	) !current_setup_index;
-
-
-
-	let setup_index_list = ref [|!current_setup_index|] in
-
-	print_message Verbose_low (" current increased " );
-	
-	while not (compare !current_setup_index !max_index) do
-		
-		current_setup_index := increase !current_setup_index !max_index;
-		
-		setup_index_list := Array.append !setup_index_list [|!current_setup_index|];
-		
-		for i = 0 to (Array.length !max_index -1) do
-  		
-  			print_message Verbose_low (string_of_int (!current_setup_index.(i)) );
-		
-		done;
-
+	let current_setup_index = ref (Array.make  (Array.length initial_loc_array) 0) in
+	let setup_index_list = ref [Array.copy !current_setup_index] in
+	(* let count = ref 0 in *)
+	while not (compare !current_setup_index !max_index) do 
+	(* while (!count < !number_of_setups - 1) do *)
+		current_setup_index := Array.copy (increase !current_setup_index !max_index);
+		setup_index_list := ([Array.copy !current_setup_index])@(!setup_index_list);	
+		(* count := !count + 1; *)
 	done;
-	
-
-
-
+	(* Testing - OK *)
+	(*print_message Verbose_low (" len setup_index_list " ^ string_of_int (List.length !setup_index_list) );*)
 	let global_init_location_constr = ref [] in
-
-	Array.iter ( fun setup_index -> 
-
+	List.iter ( fun setup_index -> 
 		let init_constr = ref (LinearConstraint.pxd_true_constraint()) in  
-
 		let initial_locations_list = ref [] in 
-
+		
 		for i = 0 to (Array.length setup_index -1) do
-
-			let j = !current_setup_index.(i) in 
-
+			let j = setup_index.(i) in 
 			let model_init_locs = initial_loc_array.(i) in 
-			
 			let (automaton_index, location_index, guard) = model_init_locs.(j) in 
 
+			print_message Verbose_low (" Testing new initial location.....");
+			print_message Verbose_low (" Automaton name: " ^ (model.automata_names automaton_index) );
+			print_message Verbose_low (" Initial location name: " ^ (model.location_names automaton_index location_index));
+			print_message Verbose_low (" Constraint: " ^ (LinearConstraint.string_of_pxd_linear_constraint model.variable_names (continuous_part_of_guard guard)));
+			print_message Verbose_low ("\n");
+
 			initial_locations_list := (automaton_index, location_index)::!initial_locations_list;
-  			
   			init_constr := LinearConstraint.pxd_intersection [!init_constr; continuous_part_of_guard guard]; 
-
-
-			()
-
 		done;
-
+		
 		let former_initial_location = model.initial_location in
-
   		let initial_PTA_locations = !initial_locations_list in
-
   		let discrete_values = List.map (fun discrete_index -> discrete_index , (Location.get_discrete_value former_initial_location discrete_index)) model.discrete in
-
   		let global_init_location = Location.make_location initial_PTA_locations discrete_values in
-
+  		
   		global_init_location_constr := (global_init_location,!init_constr)::(!global_init_location_constr);
-
 	
 	) !setup_index_list;
 
-
-
-	for i = 0 to (Array.length initial_loc_array -1) do
-  		
-  		print_message Verbose_low (string_of_int (!current_setup_index.(i)) );
-
-	done;
-
 !global_init_location_constr
-
+(* Testing - OK *)
 
 
 
