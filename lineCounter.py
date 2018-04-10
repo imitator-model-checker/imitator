35eef99dbf323d787967af6b6ea0e2e56ce54b61
Jaime Arias <arias@lipn.univ-paris13.fr>
2018-04-10 15:52:05 +0200

    a possible refactoring for lineCounter.py

--- lineCounter.py
+++ lineCounter.py
@@ -1,47 +1,47 @@
 ########################################################
+#   Laboratoire Specification et Verification
+#
+#   Line counter script
+#
+#   Author: Etienne ANDRE
+#
+#   Created       : 2009/04/06
+#   Last modified : 2014/09/24
 ########################################################
-##   Laboratoire Specification et Verification
-##
-##   Line counter script
-##
-##   Author: Etienne ANDRE
-##
-##   Created       : 2009/04/06
-##   Last modified : 2014/09/24
-########################################################
-######################################################## 
 
 ########################################################
-## IMPORTED MODULES
+# IMPORTED MODULES
 ########################################################
-import os, re
+from __future__ import print_function
 
+import os
+import re
 
 # Counter of lines
 counter = 0
+
 # Number of files
 nb_files = 0
-# Relative path
-path = "./src/" # os.getcwd() + 
 
-print " Counting lines in all files in " + path
+# Relative path
+path = "./src/"  # os.getcwd() +
+
+print(" Counting lines in all files in %s" % path)
 
 # Iterate on the files in the "src" directory
 for file in os.listdir(path):
-	file = path + file
-	# Only consider files
-	if os.path.isfile(file):
-		# Open file
-		file_object	= open(file, 'r')
-		string_file	= file_object.read()
+    file = os.path.join(path, file)
 
-		# Count lines
-		nb_lines	= len(re.findall("\n", string_file)) + 1
-		counter		= counter + nb_lines
-		nb_files	= nb_files + 1
-		
-		# Print on screen
-		print " File " + file + ": " + str(nb_lines) + " lines"
+    # Only consider files
+    if os.path.isfile(file):
+
+        # Count lines
+        nb_lines = open(file, 'r').read().count('\n') + 1
+        counter += nb_lines
+        nb_files += 1
+
+        # Print on screen
+        print(" File {}: {} lines".format(file, nb_lines))
 
 # Print final result
-print "IMITATOR contains " + str(counter) + " lines of code in " + str(nb_files) + " files."
+print("IMITATOR contains {} lines of code in {} files.".format(counter, nb_files))
