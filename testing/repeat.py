5b6c912d066a8e606f20777b78982db48cd77903
Jaime Arias <arias@lipn.univ-paris13.fr>
2018-04-11 17:55:29 +0200

    a possible refactoring of the repeat.py script

--- testing/repeat.py
+++ testing/repeat.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-#************************************************************
+# ************************************************************
 #
 #                       IMITATOR
 # 
@@ -11,108 +11,108 @@
 # File contributors : Étienne André
 # Created           : 2016/03/23
 # Last modified     : 2016/03/23
-#************************************************************
+# ************************************************************
 
 
-#************************************************************
+# ************************************************************
 # MODULES
-#************************************************************
-import time
+# ************************************************************
+from __future__ import print_function
 import datetime
 import os
 import sys
 import subprocess
 
-
-
-#************************************************************
+# ************************************************************
 # GENERAL CONFIGURATION
-#************************************************************
+# ************************************************************
 
 # Root path to the main IMITATOR root directory
-IMITATOR_PATH = ''
+IMITATOR_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Path to the binary directory
-BINARY_PATH = IMITATOR_PATH + 'bin/'
+BINARY_PATH = os.path.join(IMITATOR_PATH, 'bin/')
 # Path to the example directory
-EXAMPLE_PATH = IMITATOR_PATH + 'testing/testcases/'
+EXAMPLE_PATH = os.path.join(IMITATOR_PATH, 'testing/testcases/')
 
 # Name for the non-distributed binary to test
 BINARY_NAME = 'imitator'
 
 
-#************************************************************
+# ************************************************************
 # FUNCTIONS
-#************************************************************
-def make_binary(binary) :
-	return BINARY_PATH + binary
+# ************************************************************
+def make_binary(binary):
+    return os.path.join(BINARY_PATH, binary)
 
-def make_file(file_name) :
-	return EXAMPLE_PATH + file_name
+
+def make_file(file_name):
+    return os.path.join(EXAMPLE_PATH, file_name)
 
 
 orig_stdout = sys.stdout
-logfile = file('/dev/null', 'w')
-#sys.stdout = logfile
+logfile = open('/dev/null', 'w')
 
-#************************************************************
+
+# sys.stdout = logfile
+
+# ************************************************************
 # MAIN TESTING FUNCTION
-#************************************************************
+# ************************************************************
 
 def test(nb_times):
-	
-	#*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
-	# CHECK FOR THE EXISTENCE OF BINARIES
-	#*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
-	binary = make_binary(BINARY_NAME)
-	if not os.path.exists(binary) :
-		fail_with('Binary ' + binary + ' does not exist')
-		all_files_ok = False
-		
-	print('# TESTING BINARY ' + BINARY_NAME)
+    # *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+    # CHECK FOR THE EXISTENCE OF BINARIES
+    # *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+    binary = make_binary(BINARY_NAME)
+    # if not os.path.exists(binary):
+        # fail_with('Binary ' + binary + ' does not exist')
+        # all_files_ok = False
 
-	#*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
-	# TEST CASES
-	#*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
-	for i in range(1, nb_times):
-	
-		# Do not print each step
-		if i % 25 == 0:
-			print i
-		cmd = [binary] + [make_file('flipflop.imi')] + [make_file('flipflop.v0')] + ('-mode random1').split()
+    print('# TESTING BINARY ' + BINARY_NAME)
 
-		result = subprocess.call(cmd, stdout=logfile) # , stderr=logfile
-		if result != 0:
-			print('...oops! Something went wrong, it seems.')
-			print ' '.join(cmd)
-			sys.exit(1)
+    # *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+    # TEST CASES
+    # *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+    for i in range(1, nb_times):
+
+        # Do not print each step
+        if i % 25 == 0:
+            print(i)
+
+        cmd = [binary, make_file('flipflop.imi'), make_file('flipflop.v0'), '-mode random1']
+
+        result = subprocess.call(cmd, stdout=logfile)  # stderr=logfile
+        if result != 0:
+            print('...oops! Something went wrong, it seems.')
+            print(' '.join(cmd))
+            sys.exit(1)
 
 
-#************************************************************
+# ************************************************************
 # STARTING SCRIPT
-#************************************************************
+# ************************************************************
 
-#print '*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-'
-print('############################################################')
-print(' Script                                               v0.1')
-print('')
-print(' Étienne André')
-print(' LIPN, Université Paris 13, Sorbonne Paris Cité (France)')
-print('############################################################')
+# print '*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-'
+header="""
+############################################################
+ Script                                               v0.1
+ 
+ Étienne André
+ LIPN, Université Paris 13, Sorbonne Paris Cité (France)
+'############################################################
+"""
 now = datetime.datetime.now()
 print(now.strftime("%A %d. %B %Y %H:%M:%S %z"))
 
-
-
-#************************************************************
+# ************************************************************
 # TESTING
-#************************************************************
+# ************************************************************
 test(100000)
 
-#************************************************************
+# ************************************************************
 # THE END
-#************************************************************
+# ************************************************************
 
-print('')
-print('...The end!')
+print('\n...The end!')
 
 sys.exit(0)
