8e0e31ea3adeaf612582ab52514ad812d1adef07
Jaime Arias <arias@lipn.univ-paris13.fr>
2018-05-22 14:32:49 +0200

    Build documentation on travis CI

--- .travis/build.sh
+++ .travis/build.sh
@@ -7,7 +7,7 @@ if [[ "$TRAVIS_OS_NAME" = "linux" ]]; then
         libextlib-ocaml libextlib-ocaml-dev \
         libgmp-dev libgmp-ocaml libgmp-ocaml-dev libmpfr-dev \
         libppl-dev \
-        graphviz plotutils
+        graphviz plotutils texlive-latex-recommended texlive-latex-extra texlive-fonts-extra latexmk
 
     if [[ "$DISTRIBUTED" = "True" ]]; then
         sudo apt-get install -qq openmpi-bin openmpi-common libopenmpi-dev
@@ -36,3 +36,10 @@ if [[ "$DISTRIBUTED" = "False" ]]; then
 else
     sh build-patator.sh
 fi
+
+# Build documentation
+cd doc
+m4 classDiagramSimplified.m4 | dot -Tpng -o classDiagramSimplified.png
+m4 classDiagramFull.m4 | dot -Tpng -o classDiagramFull.png
+latexmk -pdf IMITATOR-not-developer-manual.tex
+latexmk -pdf IMITATOR-user-manual.tex
