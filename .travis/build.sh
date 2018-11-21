5f93298e4b5651f5f795993f5109e838dcfccb58
Jaime Arias Almeida <arias@lipn.univ-paris13.fr>
2018-11-21 11:54:43 +0100

    Fix ocamlmpi installation

--- .travis/build.sh
+++ .travis/build.sh
@@ -11,14 +11,11 @@ if [[ "$TRAVIS_OS_NAME" = "linux" ]]; then
 
     if [[ "$DISTRIBUTED" = "True" ]]; then
         sudo apt-get install -qq openmpi-bin openmpi-common libopenmpi-dev
-        git clone https://github.com/xavierleroy/ocamlmpi
-        (cd ocamlmpi; make clean; make MPIINCDIR=/usr/lib/openmpi/include; make opt; sudo make install)
-        rm -rf ocamlmpi
 
-        # Installting Bytes Package
-        git clone https://github.com/chambart/ocaml-bytes.git
-        (cd ocaml-bytes; ./configure --prefix=/usr --libdir=/usr/lib/ocaml/; make; sudo make install)
-        rm -rf ocaml-bytes
+        # installing version without Bytes package
+        git clone https://github.com/xavierleroy/ocamlmpi
+        (cd ocamlmpi; git reset --hard c6eaf91; make clean; make MPIINCDIR=/usr/lib/openmpi/include; make opt; sudo make install)
+        rm -rf ocamlmpi
     fi
 fi
 
