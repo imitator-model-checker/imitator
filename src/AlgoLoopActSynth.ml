ffdf88a9da6248fe508405b831dfb1c12a0f9872
etienneandre <etienne.andre@univ-paris13.fr>
2019-07-22 15:36:44 +0900

    Finalize ActLoopSynth

--- src/AlgoLoopActSynth.ml
+++ src/AlgoLoopActSynth.ml
@@ -8,7 +8,7 @@
  * 
  * File contributors : Étienne André
  * Created           : 2019/07/17
- * Last modified     : 2019/07/17
+ * Last modified     : 2019/07/22
  *
  ************************************************************)
 
@@ -43,7 +43,6 @@ class algoActLoopSynth =
 	method algorithm_name = "ActLoopSynth"
 	
 	
-	
 	(************************************************************)
 	(* Class methods *)
 	(************************************************************)
@@ -52,7 +51,21 @@ class algoActLoopSynth =
 	(* Detect whether a loop is accepting *)
 	(*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*)
 	method is_accepting scc =
-		raise (NotImplemented "work in progress")
+		(* Retrieve the model *)
+		let model = Input.get_model() in
+
+		(* Get unreachable locations *)
+		let unreachable_locations = match model.correctness_condition with
+			| Some (Unreachable unreachable_global_locations)
+			| Some (Reachable unreachable_global_locations)
+				-> unreachable_global_locations
+			| _ -> raise (InternalError "Reachability property expected in AlgoLoopActSynth.is_accepting scc")
+		in
+
+		(* Accepting if at least one state matches the reachability condition *)
+		List.exists (fun state_index -> 
+			State.match_unreachable_global_locations unreachable_locations (StateSpace.get_location state_space (StateSpace.get_global_location_index state_space state_index))
+		) scc
 
 
 
