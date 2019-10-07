50503566c938f84f1fc5f856682f9aa890eb0b3f
Laure Petrucci <laure.petrucci@lipn.univ-paris13.fr>
2019-10-07 16:16:44 +0200

    complete script for generating latex tables

--- generate_table.sh
+++ generate_table.sh
@@ -4,7 +4,7 @@
 # Script for generating experiments table #
 # Author: Laure Petrucci                  #
 # Version: 1.0                            #
-# Date: 13/0ç/2019                        #
+# Date: 04/10/2019                        #
 ###########################################
 
 function usage {
@@ -30,11 +30,22 @@ exp_dir="tests/acceptingExamples"
 one_result="tmp_result.txt"
 output_file=$2
 rm -f $output_file
-echo '\begin{sidewaystable}[pht]' > $output_file
+echo '\begin{table}[pht]' > $output_file
 echo '\centering' >> $output_file
+# first table
+# echo '\begin{tabular}{||l||c|c|c||*{6}{r|r|r||}}' >> $output_file
+# echo '\hline\hline' >> $output_file
+# echo '\rowcolor{cyan} & & & & \multicolumn{3}{c||}{No strategy} & \multicolumn{3}{c||}{Subsumption} & \multicolumn{3}{c||}{Ordering} & \multicolumn{3}{c||}{Lookahead} & \multicolumn{3}{c||}{Order+Look} & \multicolumn{3}{c||}{All} \\' >> $output_file
+# echo '\rowcolor{cyan}\multirow{-2}{*}{Model} & \multirow{-2}{*}{L} & \multirow{-2}{*}{X} & \multirow{-2}{*}{P} & \multicolumn{1}{c|}{c} & \multicolumn{1}{c|}{s} & \multicolumn{1}{c||}{t} & \multicolumn{1}{c|}{c} & \multicolumn{1}{c|}{s} & \multicolumn{1}{c||}{t} & \multicolumn{1}{c|}{c} & \multicolumn{1}{c|}{s} & \multicolumn{1}{c||}{t} & \multicolumn{1}{c|}{c} & \multicolumn{1}{c|}{s} & \multicolumn{1}{c||}{t} & \multicolumn{1}{c|}{c} & \multicolumn{1}{c|}{s} & \multicolumn{1}{c||}{t} & \multicolumn{1}{c|}{c} & \multicolumn{1}{c|}{s} & \multicolumn{1}{c||}{t} \\' >> $output_file
+# second table
+# echo '\begin{tabular}{||l||c|c|c||*{6}{r|r|r||}}' >> $output_file
+# echo '\hline\hline' >> $output_file
+# echo '\rowcolor{cyan} & & & & \multicolumn{3}{c||}{No strategy} & \multicolumn{3}{c||}{Subsumption} & \multicolumn{3}{c||}{Ordering} & \multicolumn{3}{c||}{Lookahead} & \multicolumn{3}{c||}{Order+Look} & \multicolumn{3}{c||}{Order+Look+Sub} \\' >> $output_file
+# echo '\rowcolor{cyan}\multirow{-2}{*}{Model} & \multirow{-2}{*}{L} & \multirow{-2}{*}{X} & \multirow{-2}{*}{P} & \multicolumn{1}{c|}{c} & \multicolumn{1}{c|}{s} & \multicolumn{1}{c||}{t} & \multicolumn{1}{c|}{c} & \multicolumn{1}{c|}{s} & \multicolumn{1}{c||}{t} & \multicolumn{1}{c|}{c} & \multicolumn{1}{c|}{s} & \multicolumn{1}{c||}{t} & \multicolumn{1}{c|}{c} & \multicolumn{1}{c|}{s} & \multicolumn{1}{c||}{t} & \multicolumn{1}{c|}{c} & \multicolumn{1}{c|}{s} & \multicolumn{1}{c||}{t} & \multicolumn{1}{c|}{c} & \multicolumn{1}{c|}{s} & \multicolumn{1}{c||}{t} \\' >> $output_file
+# third table
 echo '\begin{tabular}{||l||c|c|c||*{6}{r|r|r||}}' >> $output_file
 echo '\hline\hline' >> $output_file
-echo '\rowcolor{cyan} & & & & \multicolumn{3}{c||}{No strategy} & \multicolumn{3}{c||}{Subsumption} & \multicolumn{3}{c||}{Ordering} & \multicolumn{3}{c||}{Lookahead} & \multicolumn{3}{c||}{Order+Look} & \multicolumn{3}{c||}{All} \\' >> $output_file
+echo '\rowcolor{cyan} & & & & \multicolumn{3}{c||}{O+L+ accepting} & \multicolumn{3}{c||}{O+L+ param} & \multicolumn{3}{c||}{O+L+ zone} & \multicolumn{3}{c||}{O+L+S+ accepting} & \multicolumn{3}{c||}{O+L+S+ param} & \multicolumn{3}{c||}{O+L+S+ zone} \\' >> $output_file
 echo '\rowcolor{cyan}\multirow{-2}{*}{Model} & \multirow{-2}{*}{L} & \multirow{-2}{*}{X} & \multirow{-2}{*}{P} & \multicolumn{1}{c|}{c} & \multicolumn{1}{c|}{s} & \multicolumn{1}{c||}{t} & \multicolumn{1}{c|}{c} & \multicolumn{1}{c|}{s} & \multicolumn{1}{c||}{t} & \multicolumn{1}{c|}{c} & \multicolumn{1}{c|}{s} & \multicolumn{1}{c||}{t} & \multicolumn{1}{c|}{c} & \multicolumn{1}{c|}{s} & \multicolumn{1}{c||}{t} & \multicolumn{1}{c|}{c} & \multicolumn{1}{c|}{s} & \multicolumn{1}{c||}{t} & \multicolumn{1}{c|}{c} & \multicolumn{1}{c|}{s} & \multicolumn{1}{c||}{t} \\' >> $output_file
 echo '\hline\hline' >> $output_file
 for f in BRP coffee \
@@ -55,34 +66,89 @@ for f in BRP coffee \
 		echo -n `grep -e locations $one_result | cut -d, -f2,5,7 | sed -e 's/^ //' \
 			| sed -e 's/ locations, / \& /' | sed -e 's/ clock variables, / \& /' \
 			| sed -e 's/ parameters/ \&/' | sed -e 's/ parameter/ \& /' ` >> $output_file
-		# no strategy
-		bin/imitator -mode AccLoopSynthNDFS -explOrder NDFS -no-lookahead -no-acceptfirst -time-limit $timeout $exp_dir/$f.imi > $one_result
+# first table
+	# no strategy
+		# bin/imitator -mode AccLoopSynthNDFS -explOrder NDFS -no-lookahead -no-acceptfirst -time-limit $timeout $exp_dir/$f.imi > $one_result
+		# process_results
+		# echo ' & ' >> $output_file
+	# subsumption
+		# bin/imitator -mode AccLoopSynthNDFS -explOrder NDFSsub -no-lookahead -no-acceptfirst -time-limit $timeout $exp_dir/$f.imi > $one_result
+		# process_results
+		# echo ' & ' >> $output_file
+	# ordering
+		# bin/imitator -mode AccLoopSynthNDFS -explOrder NDFS -no-lookahead -time-limit $timeout $exp_dir/$f.imi > $one_result
+		# process_results
+		# echo ' & ' >> $output_file
+	# lookahead
+		# bin/imitator -mode AccLoopSynthNDFS -explOrder NDFS -no-acceptfirst -time-limit $timeout $exp_dir/$f.imi > $one_result
+		# process_results
+		# echo ' & ' >> $output_file
+	# lookahead + ordering
+		# bin/imitator -mode AccLoopSynthNDFS -explOrder NDFS -time-limit $timeout $exp_dir/$f.imi > $one_result
+		# process_results
+		# echo ' & ' >> $output_file
+	# lookahead + ordering + subsumption
+		# bin/imitator -mode AccLoopSynthNDFS -explOrder NDFSsub -time-limit $timeout $exp_dir/$f.imi > $one_result
+		# process_results
+		# echo '\\' >> $output_file
+# second table
+	# # no strategy
+	# 	bin/imitator -mode AccLoopSynthNDFS -explOrder layerNDFS -no-lookahead -no-acceptfirst -time-limit $timeout $exp_dir/$f.imi > $one_result
+	# 	process_results
+	# 	echo ' & ' >> $output_file
+	# # subsumption
+	# 	bin/imitator -mode AccLoopSynthNDFS -explOrder layerNDFSsub -no-lookahead -no-acceptfirst -time-limit $timeout $exp_dir/$f.imi > $one_result
+	# 	process_results
+	# 	echo ' & ' >> $output_file
+	# # ordering
+	# 	bin/imitator -mode AccLoopSynthNDFS -explOrder layerNDFS -no-lookahead -time-limit $timeout $exp_dir/$f.imi > $one_result
+	# 	process_results
+	# 	echo ' & ' >> $output_file
+	# # lookahead
+	# 	bin/imitator -mode AccLoopSynthNDFS -explOrder layerNDFS -no-acceptfirst -time-limit $timeout $exp_dir/$f.imi > $one_result
+	# 	process_results
+	# 	echo ' & ' >> $output_file
+	# # lookahead + ordering
+	# 	bin/imitator -mode AccLoopSynthNDFS -explOrder layerNDFS -time-limit $timeout $exp_dir/$f.imi > $one_result
+	# 	process_results
+	# 	echo ' & ' >> $output_file
+	# # lookahead + ordering + subsumption
+	# 	bin/imitator -mode AccLoopSynthNDFS -explOrder layerNDFSsub -time-limit $timeout $exp_dir/$f.imi > $one_result
+	# 	process_results
+	# 	echo '\\' >> $output_file
+# third table
+	# lookahead + ordering, pending=accepting
+		bin/imitator -mode AccLoopSynthNDFS -explOrder layerNDFS -pendingOrder accepting -time-limit $timeout $exp_dir/$f.imi > $one_result
 		process_results
 		echo ' & ' >> $output_file
-		# subsumption
-		bin/imitator -mode AccLoopSynthNDFS -explOrder NDFSsub -no-lookahead -no-acceptfirst -time-limit $timeout $exp_dir/$f.imi > $one_result
+	# lookahead + ordering, pending=param
+		bin/imitator -mode AccLoopSynthNDFS -explOrder layerNDFS -pendingOrder param -time-limit $timeout $exp_dir/$f.imi > $one_result
 		process_results
 		echo ' & ' >> $output_file
-		# ordering
-		bin/imitator -mode AccLoopSynthNDFS -explOrder NDFS -no-lookahead -time-limit $timeout $exp_dir/$f.imi > $one_result
+	# lookahead + ordering, pending=zone
+		bin/imitator -mode AccLoopSynthNDFS -explOrder layerNDFS -pendingOrder zone -time-limit $timeout $exp_dir/$f.imi > $one_result
 		process_results
 		echo ' & ' >> $output_file
-		# lookahead
-		bin/imitator -mode AccLoopSynthNDFS -explOrder NDFS -no-acceptfirst -time-limit $timeout $exp_dir/$f.imi > $one_result
+	# lookahead + ordering + subsumption, pending=accepting
+		bin/imitator -mode AccLoopSynthNDFS -explOrder layerNDFSsub -pendingOrder accepting -time-limit $timeout $exp_dir/$f.imi > $one_result
 		process_results
 		echo ' & ' >> $output_file
-		# lookahead + ordering
-		bin/imitator -mode AccLoopSynthNDFS -explOrder NDFS -time-limit $timeout $exp_dir/$f.imi > $one_result
+	# lookahead + ordering + subsumption, pending=param
+		bin/imitator -mode AccLoopSynthNDFS -explOrder layerNDFSsub -pendingOrder param -time-limit $timeout $exp_dir/$f.imi > $one_result
 		process_results
 		echo ' & ' >> $output_file
-		# lookahead + ordering + subsumption
-		bin/imitator -mode AccLoopSynthNDFS -explOrder NDFSsub -time-limit $timeout $exp_dir/$f.imi > $one_result
+	# lookahead + ordering + subsumption, pending=zone
+		bin/imitator -mode AccLoopSynthNDFS -explOrder layerNDFSsub -pendingOrder zone -time-limit $timeout $exp_dir/$f.imi > $one_result
 		process_results
 		echo '\\' >> $output_file
 	done
 echo '\hline\hline' >> $output_file
 echo '\end{tabular}' >> $output_file
-echo '\caption{Comparing strategies for collecting NDFS with layers}' >> $output_file
-echo '\label{tbl:exp:layers}' >> $output_file
-echo '\end{sidewaystable}' >> $output_file
+# echo '\caption{Comparing strategies for collecting NDFS without layers}' >> $output_file
+# echo '\label{tbl:exp:nolayer}' >> $output_file
+# echo '\caption{Comparing strategies for collecting NDFS with layers}' >> $output_file
+# echo '\label{tbl:exp:layer}' >> $output_file
+echo '\caption{Comparing strategies for ordering the pending list}' >> $output_file
+echo '\label{tbl:exp:pending}' >> $output_file
+echo '\end{table}' >> $output_file
 rm -f $one_result
