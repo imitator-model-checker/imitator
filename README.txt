5d6f0e92c9bc62dac9715d5008c39988f1cfdbcb
Laure Petrucci <laure.petrucci@lipn.univ-paris13.fr>
2020-01-25 17:17:11 +0100

    nicer presentation + updated README for artefact

--- README.txt
+++ README.txt
@@ -1,37 +1,42 @@
-The artifact provides the source files as well as a compiled version of imitator.
+The artefact provides the source files as well as a compiled version of imitator.
 
-********************************
-1. Compiling the tool (optional)
-********************************
-run the script:
+*********************************
+0. Move to the imitator directory
+*********************************
 
-sh ./build.sh
+All files are in the directory named imitator.
 
-Note that the artifact provides all dependencies for compiling. Additional information for building the tool can be found on the official web page (https://www.imitator.fr/download.html).
+cd imitator
 
 The specific implementation of NDFS source code is in the file: src/AlgoNDFS.ml
 
 ********************************
-2. The executable file
+1. The executable file
 ********************************
 The executable of the tool is:
 
 bin/imitator
 
 The execution without options/automata file provides a summary of usage of the tool.
-The full detailed manual is IMITATOR-user-manual.pdf
+The full detailed manual is in docs/IMITATOR-user-manual.pdf
 
 ********************************
-3. Running the experiments
+2. Running the experiments
 ********************************
-Four scripts are provided that were used for the experiments in the paper:
+Two scripts are provided that were used for the experiments in the paper:
 
-- brp_experiments.sh executes all experiments on the Bounded Retransmission Protocol in the same order as they appear in the paper
+- generate_table.sh generates table 2 contents in csv format which can be viewed in a spreadsheet tool (the delimitor used is ; which is standard).
+The script requires an output file specified with the -o option, e.g.:
 
-- generate_table[2-4].sh generates tables 2 and 3 from the paper plus an additional one containing experiments on the different orders proposed for the pending list.
-Since some examples timeout, it may be wise to comment out the lengthy examples.
-These scripts use two arguments: the timeout in seconds (set to 900 for the paper) and the filename for the produced LaTeX table.
-The tables obtained that way were modified to report only 2 decimals in timings and with colours in some cells for a better presentation in the paper.
+./generate_table.sh -o table.csv
 
-example:
-./generate_table2.sh 900 table2.tex
+The script also provides means to specify a timeout (other than the 90 seconds by default), use a subset of the models or give a list of models. The help option explains this:
+
+./generate_table.sh -h
+
+
+- brp_experiments.sh executes all experiments on the Bounded Retransmission Protocol in the same order as they appear in the paper. The output is not processed by the script, hence the user sees the partial results obtained on-the-fly by imitator. To enhance readability:
+	* each series of experiments in a section has a title with a green background
+	* the commands executed are displayed with a yellow background before execution
+	* The final results of imitator are easily located as the constraint is displayed with a black background 
+
--- brp_experiments.sh
+++ brp_experiments.sh
@@ -3,27 +3,45 @@
 ##############################
 # Script for BRP experiments #
 # Author: Laure Petrucci     #
-# Version: 1.0               #
-# Date: 04/10/2019           #
+# Version: 1.1               #
+# Date: 2020-01-25           #
 ##############################
 
+function execute_imi {
+	len="$((${#1} + 16))"
+	echo -n -e "\e[103m" ; printf " %${len}s" ; echo -e "\e[0m"
+	echo -e "\e[103m  bin/imitator $1  \e[0m"
+	echo -n -e "\e[103m" ; printf " %${len}s" ; echo -e "\e[0m"
+	bin/imitator $1
+}
+
 exp_dir="tests/BRP"
+
 # experiments of section 6.1
-echo -e '\e[42mExperiments of section 6.1\e[0m'
-bin/imitator -mode EF -mergeq -incl $exp_dir/brp_Channels.imi
-bin/imitator -mode EF -mergeq -incl $exp_dir/brp_RC.imi
-bin/imitator -mode EF -mergeq -incl $exp_dir/brp_RC_MAX3.imi
-bin/imitator -mode EF -mergeq -incl $exp_dir/brp_RC_MAX4.imi
-bin/imitator -mode EF -mergeq -incl $exp_dir/brp_RC_MAX20.imi
+echo -e '\e[42m                              \e[0m'
+echo -e '\e[42m  Experiments of section 6.1  \e[0m'
+echo -e '\e[42m                              \e[0m'
+
+execute_imi "-mode EF -mergeq -incl $exp_dir/brp_Channels.imi"
+execute_imi "-mode EF -mergeq -incl $exp_dir/brp_RC.imi"
+execute_imi "-mode EF -mergeq -incl $exp_dir/brp_RC_MAX3.imi"
+execute_imi "-mode EF -mergeq -incl $exp_dir/brp_RC_MAX4.imi"
+execute_imi "-mode EF -mergeq -incl $exp_dir/brp_RC_MAX20.imi"
+
 # experiments of section 6.2
-echo -e '\e[42mExperiments of section 6.2\e[0m'
-bin/imitator -mode AccLoopSynthNDFS -explOrder=NDFSsub $exp_dir/brp_RC.imi -depth-limit=20
-bin/imitator -mode AccLoopSynthNDFS -explOrder=NDFSsub $exp_dir/brp_RC_loop20.imi -depth-limit=40
-bin/imitator -mode AccLoopSynthNDFS -explOrder=NDFSsub $exp_dir/brp_RC_loop40.imi -depth-limit=45
+echo -e '\e[42m                              \e[0m'
+echo -e '\e[42m  Experiments of section 6.2  \e[0m'
+echo -e '\e[42m                              \e[0m'
+execute_imi "-mode AccLoopSynthNDFS -explOrder=NDFSsub $exp_dir/brp_RC.imi -depth-limit=20"
+execute_imi "-mode AccLoopSynthNDFS -explOrder=NDFSsub $exp_dir/brp_RC_loop20.imi -depth-limit=40"
+execute_imi "-mode AccLoopSynthNDFS -explOrder=NDFSsub $exp_dir/brp_RC_loop40.imi -depth-limit=45"
+
 # experiments of section 6.3
-echo -e '\e[42mExperiments of section 6.3\e[0m'
-bin/imitator -mode AccLoopSynthNDFS -explOrder=NDFS $exp_dir/brp_GF_S_in_RC.imi -incl
-bin/imitator -mode AccLoopSynthNDFS -explOrder=NDFS $exp_dir/brp_GF_S_in_RC2.imi -incl
-bin/imitator -mode AccLoopSynthNDFS -explOrder=NDFSsub -counterexample $exp_dir/brp_GSinFSnok.imi
-bin/imitator -mode AccLoopSynthNDFS -explOrder=NDFSsub $exp_dir/brp_GSinFSnok.imi
-bin/imitator -mode AccLoopSynthNDFS -explOrder=NDFS $exp_dir/brp_GSinFSdk.imi -incl
+echo -e '\e[42m                              \e[0m'
+echo -e '\e[42m  Experiments of section 6.3  \e[0m'
+echo -e '\e[42m                              \e[0m'
+execute_imi "-mode AccLoopSynthNDFS -explOrder=NDFS $exp_dir/brp_GF_S_in_RC.imi -incl"
+execute_imi "-mode AccLoopSynthNDFS -explOrder=NDFS $exp_dir/brp_GF_S_in_RC2.imi -incl"
+execute_imi "-mode AccLoopSynthNDFS -explOrder=NDFSsub -counterexample $exp_dir/brp_GSinFSnok.imi"
+execute_imi "-mode AccLoopSynthNDFS -explOrder=NDFSsub $exp_dir/brp_GSinFSnok.imi"
+execute_imi "-mode AccLoopSynthNDFS -explOrder=NDFS $exp_dir/brp_GSinFSdk.imi -incl"
