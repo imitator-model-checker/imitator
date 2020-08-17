ffa56cef65802f25a8c2b9c0df1d20123d97f426
Laure Petrucci <laure.petrucci@lipn.univ-paris13.fr>
2020-08-17 18:36:29 +0200

    script with new stats for layered experiments

--- generate_layer.sh
+++ generate_layer.sh
@@ -9,22 +9,30 @@
 ###########################################
 
 function usage {
-	echo -e "\033[1;31musage\033[0m: $0 [-h | [-t timeout] -o table_filename [-s | -i input_models]]"
+	echo -e "\033[1;31musage\033[0m: $0 [-h | [-t timeout] -o table_filename [-S | -i input_models]]"
 }
 
 function help {
 	echo -e ""
 	usage
-	echo -e "\nExecutes the experiments on all models (takes approximately 110 minutes). The result is written in the file specified with the \033[1m-o\033[0m option"
+	echo -e "\nExecutes the experiments on all models. The result is written in the file specified with the \033[1m-o\033[0m option"
 	echo -e "\n\033[1m-h\033[0m\t\t\tThis help"
 	echo -e "\n\033[1m-t timeout\033[0m\t\tUses a specified value for the timeout (in seconds) \033[4m[default: 90]\033[0m"
 	echo -e "\n\033[1m-o table_filename\033[0m\tOutputs the results in a csv file (with separator ;) named \033[4mtable_filename\033[0m"
-	echo -e "\n\033[1m-s\033[0m\t\t\tUses a subset of the models (takes approximately 20 minutes)"
+	echo -e "\n\033[1m-S\033[0m\t\t\tUses a subset of the models"
 	echo -e "\n\033[1m-i input_models\033[0m\t\tOnly the models in \033[4minput_models\033[0m are used"
 	exit
 }
 
 function process_results {
+	# find maximal depth
+	echo -n `grep "depth actually" $one_result | cut -d: -f2 | sed -e 's/\[0m//'`' ;'>> $output_file
+	# minimal depth at which a cycle was found
+	min_depth=`grep "Minimum depth" $one_result | cut -d: -f2 | sed -e 's/\[0m//'`
+	if [ -z "$min_depth" ]
+	then echo -n " --- ; " >> $output_file
+	else echo -n "$min_depth ; " >> $output_file
+	fi
 	echo -n `grep "cycles found" $one_result | grep -v Total | cut -d: -f2 | sed -e 's/\[0m//'`' ; '>> $output_file
 	echo -n `grep "processed states" $one_result | grep -v Total | cut -d: -f2 | sed -e 's/\[0m//'`' ; ' >> $output_file
 	exceeds_time=`grep "under-approximation" $one_result`
@@ -52,12 +60,12 @@ input_files="BRP coffee \
 		Sched2.50.2 simop \
 		spsmall tgcTogether2 \
 		WFAS-BBLS15-det"
-while getopts "ht:o:si:'" opt; do
+while getopts "ht:o:Si:'" opt; do
 case $opt in
 	h) help ;;
 	t) timeout=$OPTARG ;;
 	o) output_file=$OPTARG ;;
-	s) input_files="critical-region critical-region4 F3 F4 FDDI4 FischerAHV93 flipflop fmtv1A1-v2 \
+	S) input_files="critical-region critical-region4 F3 F4 FDDI4 FischerAHV93 flipflop fmtv1A1-v2 \
 		lynch lynch5 \
 		Pipeline-KP12-2-3 \
 		RCP Sched2.100.0 \
@@ -78,7 +86,7 @@ rm -f $one_result
 rm -f $output_file
 # table with layers
 echo ' ; ; ; ; No strategy ; ; ; Subsumption ; ; ; Ordering ; ; ; Lookahead ; ; ; Order+Look ; ; ; O + L + Sub ; ;' > $output_file
-echo 'Model ; L ; X ; P ; c ; s ; t ; c ; s ; t ; c ; s ; t ; c ; s ; t ; c ; s ; t ; c ; s ; t' >> $output_file
+echo 'Model ; L ; X ; P ; d ; m ; c ; s ; t ; d ; m ; c ; s ; t ; d ; m ; c ; s ; t ; d ; m ; c ; s ; t ; d ; m ; c ; s ; t ; d ; m ; c ; s ; t' >> $output_file
 for f in $input_files
 	do echo -e "Running experiments for model \033[1;31m$f\033[0m"
 		bin/imitator -mode checksyntax -verbose low $exp_dir/$f.imi > $one_result 2> /dev/null
