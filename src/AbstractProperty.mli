e8183afa6b33b3bc2870cb368e860c684616ab2e
Étienne André <Andre.Etienne@lipn13.fr>
2020-08-25 17:39:17 +0200

    Cleanup

--- src/AbstractProperty.mli
+++ src/AbstractProperty.mli
@@ -8,7 +8,7 @@
  *
  * File contributors : Étienne André
  * Created           : 2019/10/08
- * Last modified     : 2020/08/20
+ * Last modified     : 2020/08/25
  *
  ************************************************************)
 
@@ -55,12 +55,6 @@ and state_predicate =
 (************************************************************)
 (** Definition of property *)
 (************************************************************)
-(*type abstract_algorithm =
-	(** Reachability *)
-	| Reachability of state_predicate
-	(** Safety *)
-	| Safety of state_predicate
-	*)
 
 type duration = LinearConstraint.p_linear_term
 
@@ -183,19 +177,7 @@ type property =
 	| PRPC of state_predicate * HyperRectangle.hyper_rectangle
 
 
-(*	(*------------------------------------------------------------*)
-	(* Observer patterns *)
-	(*------------------------------------------------------------*)
-	
-	(* if a2 then a1 has happened before *)
-	| Action_precedence_acyclic of action_index * action_index
-	(* everytime a2 then a1 has happened before *)
-	| Action_precedence_cyclic of action_index * action_index
-	(* everytime a2 then a1 has happened exactly once before *)
-	| Action_precedence_cyclicstrict of action_index * action_index*)
 
-
-	
 
 type synthesis_type =
 	(* (tentative) exhibition of at least one valuation for which a property holds *)
@@ -204,14 +186,7 @@ type synthesis_type =
 	| Synthesis
 
 
-(* 	type correctness_condition = reachability_property option *)
-
 type projection = (parameter_index list) option
-
-(*type optimization =
-  | No_optimization
-  | Minimize of parameter_index
-  | Maximize of parameter_index*)
 
   
 (************************************************************)
@@ -226,54 +201,5 @@ type abstract_property = {
 	(* Projection of the result *)
 	projection		: projection;
 }
-
-
-
-(*
-
-
-(** Definition of the property by the end user *)
-type property =
-  (* DEPRECATED *)
-  (* 	| Exists_action of action_index *)
-
-  (* An "OR" list of global locations *)
-  | Unreachable_locations of unreachable_global_location list
-
-  (*** NOTE: not implemented ***)
-  (*	(* if a1 then eventually a2 *)
-    	| Eventual_response_acyclic of action_index * action_index
-    	(* everytime a1 then eventually a2 *)
-    	| Eventual_response_cyclic of action_index * action_index
-    	(* everytime a1 then eventually a2 once before next *)
-    	| Eventual_response_cyclicstrict of action_index * action_index*)
-
-  (* a no later than d *)
-  | Action_deadline of action_index * duration
-
-  (* if a2 then a1 happened within d before *)
-  | TB_Action_precedence_acyclic of action_index * action_index * duration
-  (* everytime a2 then a1 happened within d before *)
-  | TB_Action_precedence_cyclic of action_index * action_index * duration
-  (* everytime a2 then a1 happened once within d before *)
-  | TB_Action_precedence_cyclicstrict of action_index * action_index * duration
-
-  (* if a1 then eventually a2 within d *)
-  | TB_response_acyclic of action_index * action_index * duration
-  (* everytime a1 then eventually a2 within d *)
-  | TB_response_cyclic of action_index * action_index * duration
-  (* everytime a1 then eventually a2 within d once before next *)
-  | TB_response_cyclicstrict of action_index * action_index * duration
-
-  (* sequence: a1, ..., an *)
-  | Sequence_acyclic of action_index list
-  (* sequence: always a1, ..., an *)
-  | Sequence_cyclic of action_index list
-
-  (* Would be better to have an "option" type *)
-  | Noproperty
-
-
-*)
 
 
