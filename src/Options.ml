fc29bc624d6a0046e7e7531cbfc01d348953fd75
Laure Petrucci <laure.petrucci@lipn.univ-paris13.fr>
2020-08-28 18:09:19 +0200

    print the chosen pending list order

--- src/Options.ml
+++ src/Options.ml
@@ -1143,7 +1143,16 @@ end;
 				print_message Verbose_standard ("No ordering of pending list with larger zones first in NDFS synthesis.")
 			else
 				print_message Verbose_medium ("Ordering pending list with larger zones first in NDFS synthesis (default)."); *)
-
+			begin
+			match pending_order with
+			| Pending_accept ->
+				print_message Verbose_standard ("Ordering pending list with accepting states first.")
+			| Pending_param ->
+				print_message Verbose_standard ("Ordering pending list with larger parameter zones first.")
+			| Pending_zone ->
+				print_message Verbose_standard ("Ordering pending list with larger zones first.")
+			| Pending_none -> ()
+			end;
 
 			(*** TODO: check that only in IM/BC mode ***)
 			if !no_random then
