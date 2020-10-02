b081aecbcd77c84a0b85193e174007c0095f7ae8
Laure Petrucci <laure.petrucci@lipn.univ-paris13.fr>
2020-10-02 21:06:11 +0200

    Brp experiments files

--- brp_experiments.sh
+++ brp_experiments.sh
@@ -19,7 +19,7 @@ exp_dir="tests/BRP"
 
 # experiments of section 6.1
 echo -e '\e[42m                              \e[0m'
-echo -e '\e[42m  Experiments of section 6.1  \e[0m'
+echo -e '\e[42m  Experiments of section 7.1  \e[0m'
 echo -e '\e[42m                              \e[0m'
 
 execute_imi "-mergeq -comparison inclusion $exp_dir/brp_Channels.imi $exp_dir/brp_Channels.imiprop"
@@ -31,17 +31,17 @@ execute_imi "-mergeq -comparison inclusion $exp_dir/brp_RC_MAX4.imi $exp_dir/brp
 
 # experiments of section 6.2
 echo -e '\e[42m                              \e[0m'
-echo -e '\e[42m  Experiments of section 6.2  \e[0m'
+echo -e '\e[42m  Experiments of section 7.2  \e[0m'
 echo -e '\e[42m                              \e[0m'
 execute_imi "$exp_dir/brp_RC.imi $exp_dir/accepting.imiprop -depth-limit=20"
-execute_imi "$exp_dir/brp_RC_R1.imi $exp_dir/accepting.imiprop -depth-init=10 -depth-step=5"
 #LP: strange results for the next two lines
-# execute_imi "$exp_dir/brp_RC_loop20.imi $exp_dir/accepting.imiprop -depth-limit=40"
-# execute_imi "$exp_dir/brp_RC_loop40.imi $exp_dir/accepting.imiprop -depth-limit=45"
+execute_imi "$exp_dir/brp_RC_loop20.imi $exp_dir/accepting.imiprop -depth-limit=40"
+execute_imi "$exp_dir/brp_RC_loop40.imi $exp_dir/accepting.imiprop -depth-limit=45"
+execute_imi "$exp_dir/brp_RC.imi $exp_dir/accepting.imiprop -depth-init=10 -depth-step=5 -depth-limit=25 -recompute-green"
 
 # experiments of section 6.3
 echo -e '\e[42m                              \e[0m'
-echo -e '\e[42m  Experiments of section 6.3  \e[0m'
+echo -e '\e[42m  Experiments of section 7.3  \e[0m'
 echo -e '\e[42m                              \e[0m'
 execute_imi "-no-subsumption -comparison inclusion $exp_dir/brp_GF_S_in_RC.imi $exp_dir/accepting.imiprop"
 execute_imi "-no-subsumption -comparison inclusion $exp_dir/brp_GF_S_in_RC2.imi $exp_dir/accepting.imiprop"
