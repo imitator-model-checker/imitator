c141940c82d0940172148989dbe4503f64be4f4f
Jaime Arias Almeida <arias@lipn.univ-paris13.fr>
2021-07-20 15:16:00 +0200

    ci: fix github actions

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,7 +3,7 @@
 cd bin
 
 if [[ "$RUNNER_OS" = "Linux" ]]; then
-    if [[ "$DISTRIBUTED" = "True" ]]; then
+    if [ -f "patator" ]; then
         mv "patator" "patator-$TAG-amd64"
     else
         mv "imitator" "imitator-$TAG-amd64"
