fa9b9f532265d88e47684f89ba59051685576b37
Étienne André <Andre.Etienne@lipn13.fr>
2023-06-21 08:04:31 +0200

    Remove commented code

--- src/lib/ModelConverter.mli
+++ src/lib/ModelConverter.mli
@@ -35,12 +35,3 @@ exception InvalidProperty
 (****************************************************************)
 (** Convert the parsed model and the parsed property into an abstract model and an abstract property *)
 val abstract_structures_of_parsing_structures : Options.imitator_options -> ParsingStructure.parsed_model -> (ParsingStructure.parsed_property option) -> AbstractModel.abstract_model * (AbstractProperty.abstract_property option)
-
-(** Check and convert the parsing structure into an abstract property *)
-(* val abstract_model_of_parsed_property : Options.imitator_options -> AbstractModel.abstract_model * useful_parsing_model_information -> ParsingStructure.parsed_property -> ImitatorUtilities.synthesis_algorithm *)
-
-(*(** Check and convert the parsed reference parameter valuation into an abstract representation *)
-val check_and_make_pi0 : ParsingStructure.pi0 -> (*Options.imitator_options ->*) PVal.pval
-
-(** Check and convert the parsed hyper-rectangle into an abstract representation *)
-val check_and_make_v0 : ParsingStructure.v0 -> (*Options.imitator_options ->*) HyperRectangle.hyper_rectangle*)
