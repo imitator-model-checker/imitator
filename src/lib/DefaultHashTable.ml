aafde952f404f3129875532fa09366e25ba8b4f3
mikaelbdj <mikaelbdj@gmail.com>
2025-05-27 15:26:17 +0200

    Fix bug in merge function of DefaultHashTable
    If a merger index was not in the hash table errors occured. Now if a
    merge index is not present it will be added and merged into.

--- src/lib/DefaultHashTable.ml
+++ src/lib/DefaultHashTable.ml
@@ -25,17 +25,20 @@ class ['a, 'b] defaultHashTable (default : 'a -> 'b) = object
   method is_empty = Hashtbl.length internal_tbl = 0
   (* Merges keys according to a mapping (mergee -> merger) 
       along with a function describing how to merge their value (for example list concatenation if values are lists) *)
-  method merge_keys (key_mapping : 'a -> 'a) (value_merger : 'b -> 'b -> 'b) = 
+  method merge_keys (key_mapping : 'a -> 'a) (merge_values : 'b -> 'b -> 'b) = 
     let new_tbl = Hashtbl.create 100 in 
     let rec lookup_key_mapping elem = 
       let res = key_mapping elem  in if res = elem then elem else lookup_key_mapping res in 
     Hashtbl.iter (
-      fun key value -> 
+      fun key old_value -> 
         let merger = lookup_key_mapping key in 
         if merger = key then
-          Hashtbl.add new_tbl key value
+          Hashtbl.add new_tbl key old_value
         else 
-          Hashtbl.add new_tbl merger (value_merger (Hashtbl.find internal_tbl merger) (Hashtbl.find internal_tbl key))
+          let new_value = match Hashtbl.find_opt internal_tbl merger with 
+          | Some value -> merge_values value old_value
+          | None -> old_value in 
+          Hashtbl.add new_tbl merger new_value
     )
     internal_tbl
 end
