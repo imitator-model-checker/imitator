92e627f3fca34bbd4a533368a8c9ca3424d1bc1b
mikaelbdj <mikaelbdj@gmail.com>
2025-10-23 16:10:56 +0200

    Rename missed variables

--- src/bin/validator/ModelGen.ml
+++ src/bin/validator/ModelGen.ml
@@ -26,43 +26,43 @@ module SimpleModel = struct
   }
 
 
-  let const_int_from_bytes br = 
-    let sample = Sampler.next_int br 10 in 
+  let const_int_from_sampler sampler = 
+    let sample = Sampler.next_int sampler 10 in 
     SConstant sample
 
-  let variable_from_bytes br ~nb_clocks ~nb_parameters = 
-    let coinflip = Sampler.next_bool br ~prob:0.5 in
+  let variable_from_sampler sampler ~nb_clocks ~nb_parameters = 
+    let coinflip = Sampler.next_bool sampler ~prob:0.5 in
     if nb_parameters = 0 || coinflip then 
-      let sample = Sampler.next_int br nb_clocks in 
+      let sample = Sampler.next_int sampler nb_clocks in 
       SClock sample
     else
-      let sample = Sampler.next_int br nb_parameters in 
+      let sample = Sampler.next_int sampler nb_parameters in 
       SParam sample
     
     
 
-  let comparison_from_bytes br ~nb_clocks ~nb_parameters ~opers = 
-    let clock = variable_from_bytes br ~nb_clocks ~nb_parameters in 
-    let constant = const_int_from_bytes br in 
-    let oper = Sampler.sample_uniform br ~from:opers in 
+  let comparison_from_sampler sampler ~nb_clocks ~nb_parameters ~opers = 
+    let clock = variable_from_sampler sampler ~nb_clocks ~nb_parameters in 
+    let constant = const_int_from_sampler sampler in 
+    let oper = Sampler.sample_uniform sampler ~from:opers in 
     SComp (clock, oper, constant)
 
-  let bool_expr_from_bytes br ~nb_clocks ~nb_parameters ~opers = 
-    let comparison = comparison_from_bytes br ~nb_clocks ~nb_parameters ~opers in
+  let bool_expr_from_sampler sampler ~nb_clocks ~nb_parameters ~opers = 
+    let comparison = comparison_from_sampler sampler ~nb_clocks ~nb_parameters ~opers in
     [comparison]
     
-  let resets_from_bytes br nb_clocks =
+  let resets_from_sampler sampler nb_clocks =
     (* each clock has an independent chance to be added to the list of resets *)
-    let should_add_reset () = Sampler.next_bool br ~prob:0.1 in
+    let should_add_reset () = Sampler.next_bool sampler ~prob:0.1 in
     let rec compute_resets acc = function
     | 0 -> acc
     | n -> if should_add_reset () then compute_resets ((n - 1)  :: acc) (n - 1) else acc in 
     compute_resets [] nb_clocks
 
-  let transition_from_bytes br ~nb_clocks ~nb_parameters =
-    let guard = bool_expr_from_bytes br ~nb_clocks ~nb_parameters ~opers:[EQ; GEQ; G; L; LEQ] in 
-    let controllable = Sampler.next_bool br ~prob:0.5 in 
-    let resets = resets_from_bytes br nb_clocks in
+  let transition_from_sampler sampler ~nb_clocks ~nb_parameters =
+    let guard = bool_expr_from_sampler sampler ~nb_clocks ~nb_parameters ~opers:[EQ; GEQ; G; L; LEQ] in 
+    let controllable = Sampler.next_bool sampler ~prob:0.5 in 
+    let resets = resets_from_sampler sampler nb_clocks in
     {controllable; guard; resets}
 
   (* 
@@ -78,7 +78,7 @@ module SimpleModel = struct
     num /. denom  
 
 
-  let accepting_locations_from_bytes ~nb_auto ~nb_loc_of_automaton ~sampler ~guarantee_accepting  =
+  let accepting_locations_from_sampler ~nb_auto ~nb_loc_of_automaton ~sampler ~guarantee_accepting  =
     Array.init nb_auto (fun i ->
       let accepting_loc_exists = ref false in 
       Array.init nb_loc_of_automaton.(i) (fun j ->
@@ -89,26 +89,26 @@ module SimpleModel = struct
         accepting || forced_accepting 
       ))
 
-  let invariants_from_bytes ~nb_auto ~nb_loc_of_automaton ~sampler ~nb_clocks ~nb_parameters = 
+  let invariants_from_sampler ~nb_auto ~nb_loc_of_automaton ~sampler ~nb_clocks ~nb_parameters = 
     Array.init nb_auto (fun i -> 
       Array.init nb_loc_of_automaton.(i) (fun _ ->
-        bool_expr_from_bytes sampler ~nb_clocks ~nb_parameters ~opers:[L; LEQ]
+        bool_expr_from_sampler sampler ~nb_clocks ~nb_parameters ~opers:[L; LEQ]
       ))
 
-  let adjacency_matrix_from_bytes ~rows ~columns ~sampler ~density ~nb_clocks ~nb_parameters  =
+  let adjacency_matrix_from_sampler ~rows ~columns ~sampler ~density ~nb_clocks ~nb_parameters  =
     let matrix : (transition option) array array = Array.make_matrix rows columns None in 
 
     (* Spanning tree *)
     let parents = ref [0] in
     for i = 1 to rows - 1 do 
       let parent = Sampler.sample_uniform sampler ~from:!parents in
-      let trans = transition_from_bytes sampler ~nb_clocks ~nb_parameters in 
+      let trans = transition_from_sampler sampler ~nb_clocks ~nb_parameters in 
       matrix.(parent).(i) <- Some trans;
       parents := i::!parents
     done;
     
     let transform = function Some x -> Some x | None -> 
-      if Sampler.next_bool sampler ~prob:density then Some (transition_from_bytes sampler ~nb_clocks ~nb_parameters) else None in  
+      if Sampler.next_bool sampler ~prob:density then Some (transition_from_sampler sampler ~nb_clocks ~nb_parameters) else None in  
     Array.map (Array.map transform) matrix
 
   let gen : t gen =                 
@@ -118,10 +118,10 @@ module SimpleModel = struct
         let nb_loc_of_automaton = Array.init nb_auto (fun _ -> Sampler.next_int sampler ~min:10 5) in 
         let matrices =
         List.init nb_auto (fun a_id ->
-          adjacency_matrix_from_bytes ~rows:(nb_loc_of_automaton.(a_id)) ~columns:(nb_loc_of_automaton.(a_id)) ~sampler ~density:0.1 ~nb_clocks ~nb_parameters)
+          adjacency_matrix_from_sampler ~rows:(nb_loc_of_automaton.(a_id)) ~columns:(nb_loc_of_automaton.(a_id)) ~sampler ~density:0.1 ~nb_clocks ~nb_parameters)
         in
-        let accepting = accepting_locations_from_bytes ~nb_auto ~nb_loc_of_automaton ~sampler ~guarantee_accepting:true in 
-        let invariants = invariants_from_bytes ~nb_auto ~nb_loc_of_automaton ~nb_clocks ~nb_parameters ~sampler in
+        let accepting = accepting_locations_from_sampler ~nb_auto ~nb_loc_of_automaton ~sampler ~guarantee_accepting:true in 
+        let invariants = invariants_from_sampler ~nb_auto ~nb_loc_of_automaton ~nb_clocks ~nb_parameters ~sampler in
         { transitions = matrices; accepting; nb_clocks; nb_parameters; invariants})
 
 end
